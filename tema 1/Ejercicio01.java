209ec1f2ffc771e582063147dd9c69477e7ec987
denise98 <ninibur@gmail.com>
2017-10-17 10:35:04 +0200

    subiendo ejercicios

---
+++ tema 1/Ejercicio01.java
@@ -0,0 +1,10 @@
+/**
+*Escribe una programa que muestre tu nombre por pantalla.
+* 
+* @author Denise Burgos García
+*/
+public class Ejercicio01 {
+  public static void main(String[] args) {
+     System.out.println("Denise Burgos García");
+  }
+}
